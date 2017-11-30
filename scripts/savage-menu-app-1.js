f8ca9835a575b201f88f77cb0c884c9ae05a48e0
dvinodeluru <dvinod.eluru@gmail.com>
2017-11-30 22:54:34 +0530

    script file refinment with click events

---
+++ scripts/savage-menu-app-1.js
@@ -0,0 +1,145 @@
+﻿/* 
+    
+    Savage Menu App
+    Version 1.0
+
+*/
+
+"use strict";
+
+var savageclose = document.getElementById('sv-menu-control'),
+    svmenubox = document.getElementById('sv-menu-box'),
+    svuserbox = document.getElementById('sv-welcomeusr'),
+    savageMenuSlider = document.getElementById('savage-header-bar'),
+    quicklinkbar = document.getElementById('sv-quicklink-bar'),
+    svQuickLink = document.getElementById('sv-quicklink'),
+    svmenuslider = document.getElementById('sv-menu-slider'),
+    svusrctrl = document.getElementById('sv-user-control'),
+    menudropdown = document.querySelectorAll('.sv-menu-dropbox'),
+    tiledropdown = document.querySelectorAll('.sv-tile-drop'),
+    tilegroup = document.getElementById('sv-menu-tilegroup'),
+    tileMenu = document.querySelectorAll('.sv-menu-tile'),
+    tilesWithoutSubgroups = document.querySelectorAll('.sv-menu-tile:not(.sv-menu-subgroup)'),
+    tileItem = document.querySelectorAll('.sv-menu-subgroup'),
+    touchevent = '';
+
+//Burger Menu event
+savageclose.addEventListener('click', function () {
+    console.log(this.classList.contains('back'));
+    if (!this.classList.contains('back')) {
+        this.classList.toggle('open');
+        savageMenuSlider.classList.toggle('open');
+        quicklinkbar.classList.remove('open');
+        svQuickLink.classList.toggle('off');
+        document.body.classList.toggle('scrolloff');
+        svQuickLink.classList.remove('on');
+    } else {
+        this.classList.remove('back');
+        tilegroup.style.left = 0;
+        this.setAttribute('data-label', 'Menu');
+    }
+});
+
+//user control dropdown
+svuserbox.addEventListener('click', function (event) {
+    this.classList.toggle('open');
+});
+
+//quick link
+svQuickLink.addEventListener('click', function () {
+    quicklinkbar.classList.toggle('open');
+    this.classList.toggle('on');
+});
+
+//Close the user control dropdown menu if the user clicks outside of it
+document.onclick = function (event) {
+    if (!event.target.matches('.sv-welcomeusr')) {
+        //console.log(event.target);
+        menudropdown.forEach(function (openDropdown) {
+            svuserbox.classList.contains('open') && svuserbox.classList.remove('open');
+        });
+    }
+    if (!event.target.matches('.sv-menu-tile')) {
+        //console.log(event.target);
+        for (var i = 0; i < tileItem.length; i++) { tileItem[i].classList.remove('open'); tileItem[i].querySelector('.sv-menu-dropbox').removeAttribute('style'); tileItem[i].parentElement.classList.remove('tileOn'); }
+    }
+};
+
+//Responsive events
+var onResizing = function (event) {
+    var winW = window.innerWidth;
+    savageclose.classList.remove('back');
+
+    tilegroup.removeAttribute('style');
+    if (winW <= 991) {
+        tilegroup.appendChild(svuserbox);
+        tilegroup.style.width = winW;
+    } else {
+        svusrctrl.insertBefore(svuserbox, svusrctrl.childNodes[0]);
+        //remove previous opend tile dropdowns
+        hideAllTiles(event);
+    }
+}
+window.onload = onResizing
+window.onresize = onResizing;
+
+// Turn off all dropdown when hovering over items with no drowpdownsOnly Click Event
+// for (var i = 0; i < tilesWithoutSubgroups.length; i++) {
+//     tilesWithoutSubgroups[i].addEventListener("mouseover", hideAllTiles, false);
+// }
+
+
+//Only Click Event
+for (var i = 0; i < tileItem.length; i++) {
+    //    tileItem[i].addEventListener("mouseover", hideAllTiles, false);
+    tileItem[i].addEventListener("click", showTile, false);
+}
+
+function hideAllTiles(e) {
+    e.stopPropagation();
+    for (var i = 0; i < tileItem.length; i++) { tileItem[i].classList.remove('open'); tileItem[i].querySelector('.sv-menu-dropbox').removeAttribute('style'); tileItem[i].parentElement.classList.remove('tileOn'); }
+}
+
+function showTile(e) {
+    e.stopPropagation();
+    var winW = window.innerWidth;
+
+    // // ignore mousover on small devices [no longer needed -- no longer using mouseover]
+    // if(winW <= 991 && e.type == "mouseover"){
+    //     return;
+    // }
+    //remove previous opend tile dropdowns for Click only event
+    hideAllTiles(e);
+
+    console.log(winW);
+    this.classList.add("open");
+
+    var childElm = this.children[1], childsgl = this.querySelectorAll('.sv-tile-subgroup').length,
+        childPos = childElm.getBoundingClientRect(),
+        thisPos = this.getBoundingClientRect();
+
+    if (winW <= 991) {
+        //Slide menu in mobile
+        var dataLabel = this.querySelectorAll('[data-label]');
+        savageclose.classList.add('back');
+        savageclose.setAttribute('data-label', dataLabel[0].getAttribute('data-label'));
+        this.parentElement.style.left = -winW + 'px';
+        this.querySelector('.sv-menu-dropbox').removeAttribute('style');
+    } else {
+        //Show hide dropdown in Desktop
+        this.parentElement.classList.add('tileOn');
+        // recalculate position of tile after tile is selected (account for addition of scrollbar)
+        thisPos = this.getBoundingClientRect();
+        childPos = childElm.getBoundingClientRect();
+        this.parentElement.style.left = 0;
+        childElm.style.top = thisPos.top + 45 + 'px';
+        if (this.offsetLeft < (tilegroup.clientWidth / 3)) {
+            childElm.style.left = (childsgl < 5) ? this.offsetLeft + 'px' : childElm.style.left = childElm.style.right = 40 + 'px';
+        } else if (thisPos.right < (tilegroup.clientWidth / 3)) {
+            childElm.style.right = (childsgl < 5) ? (tilegroup.clientWidth - (this.offsetLeft + thisPos.width)) - 40 + 'px' : childElm.style.left = childElm.style.right = 40 + 'px';
+        } else {
+            childElm.style.left = (childsgl < 5) ? ((this.offsetLeft + (thisPos.width / 2)) - (childPos.width / 2)) + 'px' : childElm.style.left = childElm.style.right = 40 + 'px';
+        }
+    }
+}
+
--- scripts/savage-menu-app.js
+++ scripts/savage-menu-app.js
@@ -6,113 +6,146 @@
 */
 
 "use strict";
-var savageclose = document.getElementById('sv-menu-control'),
-    svmenubox = document.getElementById('sv-menu-box'),
-    svuserbox = document.getElementById('sv-welcomeusr'),
-    savageMenuSlider = document.getElementById('savage-header-bar'),
-    quicklinkbar = document.getElementById('sv-quicklink-bar'),
-    svQuickLink = document.getElementById('sv-quicklink'),
-    svmenuslider = document.getElementById('sv-menu-slider'),
-    svusrctrl = document.getElementById('sv-user-control'),
-    menudropdown = document.querySelectorAll('.sv-menu-dropbox'),
-    tiledropdown = document.querySelectorAll('.sv-tile-drop'),
-    tilegroup = document.getElementById('sv-menu-tilegroup'),
-    tileMenu = document.querySelectorAll('.sv-menu-tile'),
-    tileItem = document.querySelectorAll('.sv-menu-subgroup'),
-    touchevent = '';
 
-//Burger Menu event
-savageclose.addEventListener('click', function () {
-    console.log(this.classList.contains('back'));
-    if (!this.classList.contains('back')) {
-        this.classList.toggle('open');
-        savageMenuSlider.classList.toggle('open');
-        quicklinkbar.classList.remove('open');
-        svQuickLink.classList.toggle('off');
-        document.body.classList.toggle('scrolloff');
-        svQuickLink.classList.remove('on');
-    } else {
-        this.classList.remove('back');
-        tilegroup.style.left = 0;
-        this.setAttribute('data-label', 'Menu');
-    }
-});
+var namespaceCreator = function () {
+    //private properties
+    var savageclose = document.getElementById('sv-menu-control'),
+        svmenubox = document.getElementById('sv-menu-box'),
+        svuserbox = document.getElementById('sv-welcomeusr'),
+        savageMenuSlider = document.getElementById('savage-header-bar'),
+        quicklinkbar = document.getElementById('sv-quicklink-bar'),
+        svQuickLink = document.getElementById('sv-quicklink'),
+        svmenuslider = document.getElementById('sv-menu-slider'),
+        svusrctrl = document.getElementById('sv-user-control'),
+        menudropdown = document.querySelectorAll('.sv-menu-dropbox'),
+        tiledropdown = document.querySelectorAll('.sv-tile-drop'),
+        tilegroup = document.getElementById('sv-menu-tilegroup'),
+        tileMenu = document.querySelectorAll('.sv-menu-tile'),
+        tilesWithoutSubgroups = document.querySelectorAll('.sv-menu-tile:not(.sv-menu-subgroup)'),
+        tileItem = document.querySelectorAll('.sv-menu-subgroup'),
+        touchevent = '';
 
-//user control dropdown
-svuserbox.addEventListener('click', function (event) {
-    this.classList.toggle('open');
-});
-
-//quick link
-svQuickLink.addEventListener('click', function () {
-    quicklinkbar.classList.toggle('open');
-    this.classList.toggle('on');
-});
-
-//Close the user control dropdown menu if the user clicks outside of it
-document.onclick = function (event) {
-    if (!event.target.matches('.sv-welcomeusr')) {
-        //console.log(event.target);
-        menudropdown.forEach(function (openDropdown) {      
-            svuserbox.classList.contains('open') && svuserbox.classList.remove('open');
-        });
-    }
-    if (!event.target.matches('.sv-tile-title')) {
-        //console.log(event.target);
-        for (var i = 0; i < tileItem.length; i++) { tileItem[i].classList.remove('open'); tileItem[i].querySelector('.sv-menu-dropbox').removeAttribute('style'); }
-    }
-};
-
-//Responsive events
-var onResizing = function (event) {
-    var winW = window.innerWidth;
-    savageclose.classList.remove('back');
-    tilegroup.removeAttribute('style');
-    if (winW <= 991) {
-        tilegroup.appendChild(svuserbox);
-        tilegroup.style.width = winW;      
-    } else {
-        svusrctrl.insertBefore(svuserbox, svusrctrl.childNodes[0]);       
-    }
-}
-window.onload = onResizing
-window.onresize = onResizing;
-
-//Only Click Event
-for (var i = 0; i < tileItem.length; i++) {
-    tileItem[i].addEventListener("click", showTile, false);
-}
-
-function showTile(e) {
-    e.stopPropagation();
-    var winW = window.innerWidth;
-    //remove previous opend tile dropdowns for Click only event
-    for (var i = 0; i < tileItem.length; i++) { tileItem[i].classList.remove('open'); tileItem[i].querySelector('.sv-menu-dropbox').removeAttribute('style'); }
-    console.log(winW);
-    this.classList.add("open");
-    
-    var childElm = this.children[1], childsgl = this.querySelectorAll('.sv-tile-subgroup').length,
-        childPos = childElm.getBoundingClientRect(),
-        thisPos = this.getBoundingClientRect();
-
-    if (winW <= 991) {
-        //Slide menu in mobile
-        var dataLabel = this.querySelectorAll('[data-label]');        
-        savageclose.classList.add('back');
-        savageclose.setAttribute('data-label', dataLabel[0].getAttribute('data-label'));
-        this.parentElement.style.left = - winW + 'px';
-        this.querySelector('.sv-menu-dropbox').removeAttribute('style');
-    } else {
-        //Show hide dropdown in Desktop
-        this.parentElement.style.left = 0;        
-        childElm.style.top = thisPos.top + 45 + 'px';
-        if (this.offsetLeft < (tilegroup.clientWidth / 3)) {
-            childElm.style.left = (childsgl < 5) ? this.offsetLeft + 'px' : childElm.style.left = childElm.style.right = 40 + 'px';
-        } else if (thisPos.right < (tilegroup.clientWidth / 3)) {
-            childElm.style.right = (childsgl < 5) ? (tilegroup.clientWidth - (this.offsetLeft + thisPos.width)) - 40 + 'px' : childElm.style.left = childElm.style.right = 40 + 'px';
+    //Burger Menu event
+    savageclose.addEventListener('click', function () {
+        console.log(this.classList.contains('back'));
+        if (!this.classList.contains('back')) {
+            this.classList.toggle('open');
+            savageMenuSlider.classList.toggle('open');
+            quicklinkbar.classList.remove('open');
+            svQuickLink.classList.toggle('off');
+            document.body.classList.toggle('scrolloff');
+            svQuickLink.classList.remove('on');
         } else {
-            childElm.style.left = (childsgl < 5) ? ((this.offsetLeft + (thisPos.width / 2)) - (childPos.width / 2)) + 'px' : childElm.style.left = childElm.style.right = 40 + 'px';
+            this.classList.remove('back');
+            tilegroup.style.left = 0;
+            this.setAttribute('data-label', 'Menu');
         }
-    }    
+    });
+
+    //user control dropdown
+    svuserbox.addEventListener('click', function (event) {
+        this.classList.toggle('open');
+    });
+
+    //quick link
+    svQuickLink.addEventListener('click', function () {
+        quicklinkbar.classList.toggle('open');
+        this.classList.toggle('on');
+    });
+
+    //Close the user control dropdown menu if the user clicks outside of it
+    document.onclick = function (event) {
+        if (!event.target.matches('.sv-welcomeusr')) {
+            //console.log(event.target);
+            menudropdown.forEach(function (openDropdown) {      
+                svuserbox.classList.contains('open') && svuserbox.classList.remove('open');
+            });
+        }
+        if (!event.target.matches('.sv-menu-tile')) {
+            //console.log(event.target);
+            for (var i = 0; i < tileItem.length; i++) { tileItem[i].classList.remove('open'); tileItem[i].querySelector('.sv-menu-dropbox').removeAttribute('style'); tileItem[i].parentElement.classList.remove('tileOn');}
+        }
+    };
+
+    //Responsive events
+    this.onResizing = function (event) {
+        var winW = window.innerWidth;
+        savageclose.classList.remove('back');
+
+        tilegroup.removeAttribute('style');
+        if (winW <= 991) {
+            tilegroup.appendChild(svuserbox);
+            tilegroup.style.width = winW;      
+        } else {
+            svusrctrl.insertBefore(svuserbox, svusrctrl.childNodes[0]);       
+            //remove previous opend tile dropdowns
+            hideAllTiles(event);
+        }
+    }
+
+    // Turn off all dropdown when hovering over items with no drowpdownsOnly Click Event
+    // for (var i = 0; i < tilesWithoutSubgroups.length; i++) {
+    //     tilesWithoutSubgroups[i].addEventListener("mouseover", hideAllTiles, false);
+    // }
+
+    //Only Click Event
+    for (var i = 0; i < tileItem.length; i++) {
+    //    tileItem[i].addEventListener("mouseover", hideAllTiles, false);
+        tileItem[i].addEventListener("click", showTile, false);
+    }
+
+    function hideAllTiles(e){
+        e.stopPropagation();
+        for (var i = 0; i < tileItem.length; i++) { tileItem[i].classList.remove('open'); tileItem[i].querySelector('.sv-menu-dropbox').removeAttribute('style'); tileItem[i].parentElement.classList.remove('tileOn'); }
+    }
+
+    function showTile(e) {
+        e.stopPropagation();
+        var winW = window.innerWidth;
+        
+        // // ignore mousover on small devices [no longer needed -- no longer using mouseover]
+        // if(winW <= 991 && e.type == "mouseover"){
+        //     return;
+        // }
+        //remove previous opend tile dropdowns for Click only event
+        hideAllTiles(e);
+        
+        console.log(winW);
+        this.classList.add("open");
+        
+        var childElm = this.children[1], childsgl = this.querySelectorAll('.sv-tile-subgroup').length,
+            childPos = childElm.getBoundingClientRect(),
+            thisPos = this.getBoundingClientRect();
+
+        if (winW <= 991) {
+            //Slide menu in mobile
+            var dataLabel = this.querySelectorAll('[data-label]');        
+            savageclose.classList.add('back');
+            savageclose.setAttribute('data-label', dataLabel[0].getAttribute('data-label'));
+            this.parentElement.style.left = - winW + 'px';
+            this.querySelector('.sv-menu-dropbox').removeAttribute('style');
+        } else {
+            //Show hide dropdown in Desktop
+            this.parentElement.classList.add('tileOn');
+            // recalculate position of tile after tile is selected (account for addition of scrollbar)
+            thisPos = this.getBoundingClientRect();
+            childPos = childElm.getBoundingClientRect();
+            this.parentElement.style.left = 0;        
+            childElm.style.top = thisPos.top + 45 + 'px';
+            if (this.offsetLeft < (tilegroup.clientWidth / 3)) {
+                childElm.style.left = (childsgl < 5) ? this.offsetLeft + 'px' : childElm.style.left = childElm.style.right = 40 + 'px';
+            } else if (thisPos.right < (tilegroup.clientWidth / 3)) {
+                childElm.style.right = (childsgl < 5) ? (tilegroup.clientWidth - (this.offsetLeft + thisPos.width)) - 40 + 'px' : childElm.style.left = childElm.style.right = 40 + 'px';
+            } else {
+                childElm.style.left = (childsgl < 5) ? ((this.offsetLeft + (thisPos.width / 2)) - (childPos.width / 2)) + 'px' : childElm.style.left = childElm.style.right = 40 + 'px';
+            }
+        }    
+    }
 }
+
+var transloadMenu = transloadMenu || {};
+namespaceCreator.call(transloadMenu);
+
+window.onload = transloadMenu.onResizing
+window.onresize = transloadMenu.onResizing;
 
